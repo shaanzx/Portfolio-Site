78c3449b8f6590c80760128e32ef6603bb258991
Shan JAY <152029750+shaanzx@users.noreply.github.com>
2024-10-24 21:06:06 +0530

    Css done

---
+++ JS/index.js
@@ -0,0 +1,15 @@
+var typed = new Typed(".text", {
+    strings: ["FullStack Developer", " Tech Enthusiast"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+  });
+
+  var typed1 = new Typed(".text1", {
+    strings: ["Hey, I'm Lakshan Jayawardhana"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+  });
